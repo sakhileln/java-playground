743fdc3f57577d96397dedca702b196a2bc26e26
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-03 19:09:22 +0200

    Update TestPoint3D class.

--- exercise/TestPoint3D.java
+++ exercise/TestPoint3D.java
@@ -8,5 +8,11 @@ public class TestPoint3D {
     System.out.println("p3A: " + p3A.toString());
     p3A.setX(-10); p3A.setY(-20); p3A.setZ(-30);
     System.out.println("Updated p3A: " + p3A.toString());
+
+    Point3D p3B = new Point3D(30,20,10);
+    System.out.println("p3B: " + p3B.toString());
+    System.out.println("Distance between p3A and p3B: " +
+                        Point3D.distance(p3A, p3B));
+    Point3D.showInfo();
   }
 }
