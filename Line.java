234e352d5cca4494fdc9e2cb91368a35d5ccd48d
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-03 17:44:33 +0200

    Add Line class.

---
+++ Line.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import exercise.*;
+
+
+public class Line {
+  // Instance variable:
+  private Point2D endPoint1;
+  private Point2D endPoint2;
+
+  // Constructor:
+  public Line(Point2D p1, Point2D p2) {
+    endPoint1 = p1;
+    endPoint2 = p2;
+  }
+
+  // Methods
+  public Point2D getEndPoint1()      { return endPoint1; }
+  public Point2D getEndPoint2()      { return endPoint2; }
+  public void setEndPoint1(Point2D p1) { endPoint1 = p1; }
+  public void setEndPoint2(Point2D p2) { endPoint2 = p2; }
+  public double length() {
+    return Point2D.distance(endPoint1, endPoint2);
+  }
+  public String toString() {
+    return "Line[" + endPoint1 + "," + endPoint2 + "]";
+  }
+}
