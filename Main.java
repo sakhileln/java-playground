0ccb0b594c1e4fa0cb1d3d18606c217471aa3eba
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-03 18:17:55 +0200

    Add Main class.

---
+++ Main.java
@@ -0,0 +1,14 @@
+package exercise;
+
+import exercise.*;
+
+class Main {
+  public static void main(String[] args) {
+    Point2D p1 = new Point2D(1, -3);
+    System.out.println(p1.toString());
+    p1.setX(4);
+    p1.setY(2);
+    System.out.println(p1.toString());
+  }
+}
+
