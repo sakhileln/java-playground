63f58e5a1994b12bdde605b992367b45c2849441
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-03 18:19:12 +0200

    Move class usage and testing to Main class.

--- Point2D.java
+++ Point2D.java
@@ -1,15 +1,5 @@
 package exercise;
 
-class Exercise {
-  public static void main(String[] args) {
-    Point2D p1 = new Point2D(1, -3);
-    System.out.println(p1.toString());
-    p1.setX(4);
-    p1.setY(2);
-    System.out.println(p1.toString());
-  }
-}
-
 public class Point2D {
   // Class member variables
   
