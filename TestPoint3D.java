dc180dc221cb57a61ac8366b264967d6bb876961
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-03 18:45:12 +0200

    Fix ',' instead of ';'

--- TestPoint3D.java
+++ TestPoint3D.java
@@ -6,7 +6,7 @@ public class TestPoint3D {
   public static void main(String [] args) {
     Point3D p3A = new Point3D(10, 20, 30);
     System.out.println("p3A: " + p3A.toString());
-    p3A.setX(-10), p3A.setY(-20), p3A.setZ(-30);
+    p3A.setX(-10); p3A.setY(-20); p3A.setZ(-30);
     System.out.println("Updated p3A: " + p3A.toString());
   }
 }
