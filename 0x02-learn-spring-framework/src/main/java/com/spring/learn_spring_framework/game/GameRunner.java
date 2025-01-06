aaa431230a5501169c2bc0d123a9dbbbb2c2792c
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-06 02:20:37 +0200

    Add 'run' method.

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/GameRunner.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/GameRunner.java
@@ -1,6 +1,15 @@
 package com.spring.learn_spring_framework.game;
 
 public class GameRunner {
+    // Fields
+    MarioGame game;
+    // Constructor
     public GameRunner(MarioGame marioGame) {
+        this.game = marioGame;
+    }
+
+    public void run() {
+        // Recommended to use logging framewrok
+        System.out.println("Running game: " + game);
     }
 }
