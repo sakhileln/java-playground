3ace9ac5e95d24a9bf9726628a8123df945b82ad
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-06 02:32:41 +0200

    Add SuperContra class.

---
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/SuperContra.java
@@ -0,0 +1,8 @@
+package com.spring.learn_spring_framework.game;
+
+public class SuperContra {
+    public void up() { System.out.println("Up"); }
+    public void down() { System.out.println("Sit down"); }
+    public void left() { System.out.println("Go back"); }
+    public void right() { System.out.println("Shoot a bullet"); }
+}
