8c7278cb6f0565b3b738015e5c6fdbf0ab22cbc5
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-06 02:10:19 +0200

    Update application.

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/AppGamingBasic.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/AppGamingBasic.java
@@ -1,4 +1,13 @@
 package com.spring.learn_spring_framework;
 
+import com.spring.learn_spring_framework.game.MarioGame;
+
 public class AppGamingBasic {
+    public static void main(String[] args) {
+
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner(marioGame);
+        gameRunner.run();
+
+    }
 }
