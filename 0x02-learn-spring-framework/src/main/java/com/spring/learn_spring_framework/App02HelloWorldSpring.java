ef7f572d7a2400333b5a5bd71ad25bd8bdd90095
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-07 11:17:31 +0200

    Test Person bean

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/App02HelloWorldSpring.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/App02HelloWorldSpring.java
@@ -14,6 +14,8 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+        // System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean("address1"));
+        System.out.println(context.getBean("person2"));
     }
 }
