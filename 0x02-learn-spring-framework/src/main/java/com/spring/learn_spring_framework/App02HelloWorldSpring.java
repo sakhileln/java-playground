b996aa389400ec23e598371bd7917a7d521fb299
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-07 10:08:19 +0200

    Hello world using spring context.

---
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/App02HelloWorldSpring.java
@@ -0,0 +1,10 @@
+package com.spring.learn_spring_framework;
+
+import com.spring.learn_spring_framework.game.GameRunner;
+import com.spring.learn_spring_framework.game.PacmanGame;
+
+public class App02HelloWorldSpring {
+    public static void main(String[] args) {
+
+    }
+}
