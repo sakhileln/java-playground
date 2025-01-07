56c5f1a7294ca929d0194ab063732c68a69bc22c
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-07 11:17:16 +0200

    Add `Address` to `Person` record.

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/HelloWorldConfiguration.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,8 +3,8 @@ package com.spring.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age) { };
-record Address (String addressLine, String city) { };
+record Person (String name, int age, Address address) { }
+record Address (String addressLine, String city) { }
 
 @Configuration
 public class HelloWorldConfiguration {
@@ -15,11 +15,20 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("Ndlazi", 17);
+        return new Person("Ndlazi", 17, address());
     }
 
     @Bean
+    public Person person2() {
+        return new Person("Ndlazi", 17, new Address("1 Google Drive", "Randburg"));
+    }
+
+    @Bean(name = "address2")
     public Address address() {
         return new Address("1 HyperLoop Road", "Ares Haven");
+    }
+    @Bean()
+    public Address address1() {
+        return new Address("44 Sun Drive", "Venus");
     }
 }
