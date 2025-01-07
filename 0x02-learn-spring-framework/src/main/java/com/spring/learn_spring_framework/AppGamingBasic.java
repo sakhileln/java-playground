878327027c02ffba39139e637cf1afeec90717a3
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-07 09:18:18 +0200

    Test interface usage.

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/AppGamingBasic.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/AppGamingBasic.java
@@ -2,13 +2,14 @@ package com.spring.learn_spring_framework;
 
 import com.spring.learn_spring_framework.game.GameRunner;
 import com.spring.learn_spring_framework.game.SuperContraGame;
+import com.spring.learn_spring_framework.game.MarioGame;
 
 public class AppGamingBasic {
     public static void main(String[] args) {
 
-        // var marioGame = new MarioGame();
-        var superContraGame = new SuperContraGame();
-        var gameRunner = new GameRunner(superContraGame);
+        var game = new MarioGame();
+        //var game = new SuperContraGame();
+        var gameRunner = new GameRunner(game);
         gameRunner.run();
 
     }
