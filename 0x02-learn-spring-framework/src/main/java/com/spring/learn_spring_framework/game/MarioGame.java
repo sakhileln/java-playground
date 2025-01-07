d5e996e45d2c9c63560675bbaf254b6ad52afe12
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-07 09:18:04 +0200

    Use interface to run game.

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/MarioGame.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/MarioGame.java
@@ -1,6 +1,6 @@
 package com.spring.learn_spring_framework.game;
 
-public class MarioGame {
+public class MarioGame implements GamingConsole {
     public void up() { System.out.println("Jump"); }
     public void down() { System.out.println("Go into a hole"); }
     public void left() { System.out.println("Go back"); }
