cc0995e26fc54f33c64ad7668423aa584726c80e
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-07 10:57:21 +0200

    Test defined beans from `HelloWorldConfirguration`.

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/App02HelloWorldSpring.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/App02HelloWorldSpring.java
@@ -1,10 +1,19 @@
 package com.spring.learn_spring_framework;
 
-import com.spring.learn_spring_framework.game.GameRunner;
-import com.spring.learn_spring_framework.game.PacmanGame;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
+        // Launch spring context
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
+        // Configure things we want spring to manage.
+        // Done in HelloWordConfiguration
+
+        // Retreive Beans managed by Spring
+        System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address"));
     }
 }
