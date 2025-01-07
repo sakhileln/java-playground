08e050b1ba1610d92f6b2b07914ce074055cca44
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-07 10:56:49 +0200

    Update beans defined in configuration.

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/HelloWorldConfiguration.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,8 +1,25 @@
 package com.spring.learn_spring_framework;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+record Person (String name, int age) { };
+record Address (String addressLine, String city) { };
 
 @Configuration
 public class HelloWorldConfiguration {
+    @Bean
+    public String name() { return "Sakhile III"; }
+    @Bean
+    public int age() { return 13; }
+
+    @Bean
+    public Person person() {
+        return new Person("Ndlazi", 17);
+    }
+
+    @Bean
+    public Address address() {
+        return new Address("1 HyperLoop Road", "Ares Haven");
+    }
 }
