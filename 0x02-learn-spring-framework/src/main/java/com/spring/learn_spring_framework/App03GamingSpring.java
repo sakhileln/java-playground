4628d0ff25f88075a933e11b943b92deb77b503c
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-09 04:20:23 +0200

    Run `GameRunner` class using spring.

--- 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/App03GamingSpring.java
+++ 0x02-learn-spring-framework/src/main/java/com/spring/learn_spring_framework/App03GamingSpring.java
@@ -8,9 +8,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App03GamingSpring {
     public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
+        try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+            context.getBean(GamingConsole.class).up();
 
-        context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        }
 
     }
 }
