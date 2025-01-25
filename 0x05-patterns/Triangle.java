ab741f1f873a5b25a52375f7525820608cbb47d2
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-25 23:31:14 +0200

    Create Triangle.java

---
+++ 0x05-patterns/Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle {
+  public static void main(String [] args) {
+    for (short i = 0; i < 5; i++) {
+      for (short j = 0; j < i+1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
