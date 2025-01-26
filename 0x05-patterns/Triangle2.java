05511d90f995a7142cf77aba8564342f89df7fea
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-01-26 13:08:01 +0200

    Add triangle.

---
+++ 0x05-patterns/Triangle2.java
@@ -0,0 +1,10 @@
+public class Triangle2 {
+  public static void main(String [] args) {
+    for (short i = 0; i < 5; i++) {
+      for (short j = 5; j > 0; j--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
