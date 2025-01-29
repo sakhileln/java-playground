a65e1d771f635b35dde11bf32593d6fa4e6b5903
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-29 22:01:44 +0200

    Update Triangle2.java

--- 0x05-patterns/Triangle2.java
+++ 0x05-patterns/Triangle2.java
@@ -1,10 +1,16 @@
 public class Triangle2 {
-  public static void main(String [] args) {
-    for (short i = 0; i < 5; i++) {
-      for (short j = 5; j > 0; j--) {
+  public static void main(String[] args) {
+    int n = 5; // Number of rows
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < n - i; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k < i; k++) {
         System.out.print("*");
       }
       System.out.println();
     }
   }
 }
+
