b035ddd3adbbfdb0959f057072e1f48a1b8f0e87
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-05 01:55:22 +0200

    Add account class.

---
+++ 0x06_classes/sakhile/Account.java
@@ -0,0 +1,67 @@
+package sakhile;
+
+public class Account {
+
+	private int accountNumber;
+	private double balance;
+	private String name;
+	private String email;
+	private String phoneNumber;
+
+	public Account(int accountNumber, double balance, String name, String email, String phoneNumber) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public int getAccountNumber() {
+		return this.accountNumber;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getBalance() {
+		return this.balance;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void depositFunds(double funds) {
+		System.out.println("Depositing... R" + funds);
+		this.balance += funds;
+	}
+	public void withdrawFunds(double funds) {
+		if (funds < this.balance) {
+			System.out.println("Withdrawing: R" + funds);
+			this.balance -= funds;
+		} else {
+			System.out.println("Insufficient funds.");
+		}
+	}
+
+}
