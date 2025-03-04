7dc4441a80927dd3d71cb894673dc23f8a9d1f12
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-05 01:54:55 +0200

    Update main class.

--- 0x06_classes/sakhile/Main.java
+++ 0x06_classes/sakhile/Main.java
@@ -6,7 +6,12 @@ public class Main {
 
     Account acc1 = new Account(236534, 23.5, "Sakhile", "sakhi@example.com", "0825812435");
 
-    System.out.println(acc1.getBalance());
+    System.out.println("Current balance: R" + acc1.getBalance());
+    acc1.depositFunds(10.7);
+    acc1.withdrawFunds(50.1);
+    acc1.withdrawFunds(4.1);
+    System.out.println("Current balance: R" + acc1.getBalance());
+    System.out.println(acc1.getName() + " email: " + acc1.getEmail() + " Current balance is: R" + acc1.getBalance());
   
   }
 
