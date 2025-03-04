e9303cf0c1fd45ee078e8dc70d2986040fde6ab8
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-03-04 20:28:21 +0200

    Add prime numbers.

---
+++ Prime.java
@@ -0,0 +1,44 @@
+/**
+ * Prime numbers 1-100
+ * 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71
+ * 73 79 83 89 97
+ */
+
+public class Prime {
+	public static void main(String[] args) {
+		// System.out.println(isPrime(101));
+
+		int count = 1;
+		do {
+			System.out.print(isPrime(count) ? count : " ");
+			count++;
+		} while (count < 100);
+	}
+
+	// private static boolean isPrime(int number) {
+	// 	if (number <= 1) {
+	// 		return false;
+	// 	}
+	// 	for (int i = 2; i < (long) Math.sqrt(number); i++) {
+	// 		if (number % i == 0) {
+	// 			return false;
+	// 		}
+	// 	}
+
+	// 	return true;
+	// }
+	private static boolean isPrime(int n) {
+    if (n == 1) {
+      return false;
+    }
+
+    for (int i = 2; i < (long) Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+}
