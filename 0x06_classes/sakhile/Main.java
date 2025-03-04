36ed0f22f20387c32912992148a09849981db717
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-03-04 22:03:22 +0200

    Add driver main class.

---
+++ 0x06_classes/sakhile/Main.java
@@ -0,0 +1,13 @@
+package sakhile;
+
+public class Main {
+  
+  public static void main(String[] args) {
+
+    Account acc1 = new Account(236534, 23.5, "Sakhile", "sakhi@example.com", "0825812435");
+
+    System.out.println(acc1.getBalance());
+  
+  }
+
+}
