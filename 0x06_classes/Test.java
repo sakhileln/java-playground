8f14706e852e3641041dbf929a3acfce80032460
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-05 11:48:52 +0200

    Add package test class.

---
+++ 0x06_classes/Test.java
@@ -0,0 +1,10 @@
+import sakhile.Account;
+
+public class Test {
+	public static void main(String[] args) {
+		Account acc3 = new Account(76352, 11134.1, "Zethe", "zethe@apple.com", "0791416987");
+
+		System.out.println(acc3.getBalance());
+
+	}
+}
