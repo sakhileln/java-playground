8f532b259fa062c56a0597c3ca23dbc85b4f0f28
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-06 20:36:24 +0200

    Test case for 'Dog' class.

--- 0x06_classes/Test.java
+++ 0x06_classes/Test.java
@@ -1,9 +1,10 @@
 import sakhile.Account;
 import sakhile.VipCustomer;
+import sakhile.Dog;
 
 public class Test {
 	public static void main(String[] args) {
-		Account acc3 = new Account(76352, 11134.1, "Zethe", "zethe@apple.com", "0791416987");
+		/* Account acc3 = new Account(76352, 11134.1, "Zethe", "zethe@apple.com", "0791416987");
 		System.out.println(acc3.getBalance());
 
 		Account acc5 = new Account();	
@@ -16,6 +17,11 @@ public class Test {
 		VipCustomer cust1 = new VipCustomer();
 		System.out.println(cust1.getName());
 		System.out.println(cust1.getEmail());
+		*/
+
+
+		Dog dog1 = new Dog("bobby", 1, 8, 25);
+		System.out.println(dog1.getName());
 
 	}
 }
