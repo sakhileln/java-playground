e95cfcce5c8102bf3b41e47a3b92db082249ab80
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-07 20:20:50 +0200

    Add comments.

--- 0x06_classes/sakhile/GermanShepherd.java
+++ 0x06_classes/sakhile/GermanShepherd.java
@@ -1,10 +1,12 @@
 package sakhile;
 
 public class GermanShepherd extends Dog {
+
+	// Constructor
 	public GermanShepherd(String name, int size, int weight, int teeth) {
 		super(name, size, weight, 2, teeth, "silky");
 	}
-
+	// Methods
 	public void bark() {
 		System.out.println("woof woof woof!");
 	}
