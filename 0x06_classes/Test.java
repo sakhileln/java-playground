a7e19fe702d97c582245bfd702379e960a1e7a14
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-08 11:27:02 +0200

    Account tests.

--- 0x06_classes/Test.java
+++ 0x06_classes/Test.java
@@ -5,6 +5,7 @@ import sakhile.GermanShepherd;
 
 public class Test {
 	public static void main(String[] args) {
+		// Accout Tests
 		Account acc3 = new Account(76352, 11134.1, "Zethe", "zethe@apple.com", "0791416987");
 		System.out.println(acc3.getBalance());
 
