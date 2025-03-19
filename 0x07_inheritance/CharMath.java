4a65fc0fcfac5d854698fbdf465c4dd7b52b9c23
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-20 00:19:59 +0200

    feat(chars): Adding chars producse number.

---
+++ 0x07_inheritance/CharMath.java
@@ -0,0 +1,6 @@
+public class CharMath {
+    public static void main(String[] args) {
+        System.out.println('A' + 'B'); // 131 ('A' is 65, 'B' is 66)
+        System.out.println("A" + "B"); // "AB" (String concatenation)
+    }
+}
