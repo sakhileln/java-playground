290216759bf0f0f8b934b1823d7c287c3a52d966
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-20 00:26:35 +0200

    feat(float): Floating point precision.

---
+++ 0x07_inheritance/FloatingPointFun.java
@@ -0,0 +1,6 @@
+public class FloatingPointFun {
+    public static void main(String[] args) {
+        System.out.println(0.1 + 0.2 == 0.3); // false!
+        System.out.println(0.1 + 0.2); // Prints 0.30000000000000004
+    }
+}
