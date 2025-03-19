0f00edabaa5114815b151d5398268b7d943b37ed
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-20 00:39:24 +0200

    feat(str): Substring fun.

---
+++ 0x07_inheritance/SubstringTest.java
@@ -0,0 +1,7 @@
+public class SubstringTest {
+    public static void main(String[] args) {
+        String str = "abcdef";
+        str = str.substring(2, 4); // Extracts "cd"
+        System.out.println(str); // cd
+    }
+}
