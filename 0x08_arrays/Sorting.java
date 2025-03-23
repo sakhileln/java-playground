50fab3862243c4e171529e72ae4530bcd8ecddca
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-23 22:21:35 +0200

    feat(arrays): Manually sorting the array.

---
+++ 0x08_arrays/Sorting.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Sorting {
+	public static void main(String[] args) {
+		int[] randArray = getRandomArray(10);
+
+		System.out.println(Arrays.toString(randArray));
+		Arrays.sort(randArray);
+		int[] revArray = reverseArray(randArray);
+		System.out.println(Arrays.toString(randArray));
+		System.out.println(Arrays.toString(revArray));
+	}
+
+	public static int[] getRandomArray(int len) {
+		int[] newArray = new int[len];
+		Random random = new Random();
+		for (int i = 0; i < len; i++) {
+			newArray[i] = random.nextInt(-25, 25);
+		}
+
+		return newArray;
+	}
+
+	public static int[] reverseArray(int[] arr) {
+		int[] sortedArr = Arrays.copyOf(arr, arr.length);
+
+		boolean flag = true;
+		int temp;
+		while (flag) {
+			flag = false;
+			for (int i = 0; i < sortedArr.length - 1; i++) {
+				if (sortedArr[i] < sortedArr[i + 1]) {
+					temp = sortedArr[i];
+					sortedArr[i] = sortedArr[i + 1];
+					sortedArr[i + 1] = temp;
+					flag = true;
+				}
+			}
+		}
+
+		return sortedArr;
+	}
+}
