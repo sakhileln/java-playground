bd749e93c47eb1fe2fcc448647165d891e64c5e5
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-23 20:32:06 +0200

    feat(arrays): Arrays binary search.

---
+++ 0x08_arrays/Match.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Match {
+	public static void main(String[] args) {
+		String[] sArray = {"Ze", "Zethe", "Sakhile", "Jane"};
+		Arrays.sort(sArray);
+		System.out.println(Arrays.toString(sArray));
+
+		if (Arrays.binarySearch(sArray, "Sakhile") > 0) {
+			System.out.println("Sakhile found in array.");
+		} 
+
+		int[] a1 = {1, 2, 3};
+		int[] a2 = {1, 2, 3};
+		if (Arrays.equals(a1, a2)) {
+			System.out.println("Arrays are the same.");
+		} else {
+			System.out.println("Arrays are NOT the same.");
+		}
+	}
+}
