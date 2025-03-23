a69717d4523bad6535bb8434962cc7c99785e79a
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-23 20:16:11 +0200

    feat(arrays): Use 'Arrays' class methods.

--- 0x08_arrays/UsingArrays.java
+++ 0x08_arrays/UsingArrays.java
@@ -4,10 +4,21 @@ import java.util.Random;
 public class UsingArrays {
 	public static void main(String[] args) {
 		int[] newArray = getRandomArray(10);
-		System.out.println((newArray));
+		System.out.println(Arrays.toString(newArray));
 
 		Arrays.sort(newArray);
 		System.out.println(Arrays.toString(newArray));
+
+		int[] secondArray = new int[10];
+		System.out.println(Arrays.toString(secondArray));
+		Arrays.fill(secondArray, 5);
+		System.out.println(Arrays.toString(secondArray));
+		System.out.println();
+
+		int[] thirdArray = getRandomArray(15);
+		int[] fourthArray = Arrays.copyOf(thirdArray, 7);
+		System.out.println(Arrays.toString(fourthArray));
+
 	}
 
 	public static int[] getRandomArray(int len) {
@@ -15,7 +26,7 @@ public class UsingArrays {
 		int[] array = new int[len];
 
 		for (int i = 0; i < len; i++) {
-			array[i] = random.nextInt(-15, 10);
+			array[i] = random.nextInt(-20, 20);
 		}
 
 		return array;
