5d2ca3f71aee67cc4de7628edb91b87c5ffeae0a
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-27 01:10:56 +0200

    feat(arrays): Read integers from CLI and return minimum.

---
+++ 0x08_arrays/Minimum.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Minimum {
+	public static void main(String[] args) {
+		int[] numbers = readIntergers();
+		int min =  findMin(numbers);
+
+		System.out.println("Array: " + Arrays.toString(numbers));
+		System.out.println("Array minimum value: " + min);
+	}
+
+	private static int[] readIntergers() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter comma separated numbers: ");
+		String line = sc.nextLine();
+
+		String[] tokens = line.split(",");
+		int[] numbers = new int[tokens.length];
+
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = Integer.parseInt(tokens[i].trim());
+		}
+
+		return numbers;
+	}
+
+	private static int findMin(int[] numbers) {
+		int minimum = numbers[0];
+		for (int val : numbers) {
+			if (val < minimum) {
+				minimum = val;
+			}
+		}
+		return minimum;
+	}
+}
