428be3227a4f45038b7722152f57a8d65fe05dcf
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-30 14:12:40 +0200

    feat(arrays): Explore more 'ArrayList' methods.

--- 0x09_lists/MoreLists.java
+++ 0x09_lists/MoreLists.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreLists {
@@ -19,5 +21,35 @@ public class MoreLists {
 		System.out.println(nextList);
 		groceries.addAll(nextList);
 		System.out.println(groceries);
+
+		if (groceries.contains("milk")) {
+			System.out.println("List contains milk");
+		}
+
+		groceries.add("yoghurt");
+		System.out.println("First: "+ groceries.indexOf("yoghurt"));
+		System.out.println("Last: "+ groceries.lastIndexOf("yoghurt"));
+
+		groceries.removeAll(List.of("yoghurt", "pickles"));
+		System.out.println(groceries);
+
+		groceries.retainAll(List.of("cheese", "bread", "butter"));
+		System.out.println(groceries);
+
+		groceries.clear();
+		System.out.println(groceries);
+		System.out.println("isEmpty: " + groceries.isEmpty());
+
+		groceries.addAll(Arrays.asList("ham", "eggs", "milk", "bread"));
+		groceries.addAll(List.of("mustard", "cheese", "butter", "apples"));
+		System.out.println(groceries);
+
+		groceries.sort(Comparator.naturalOrder());
+		System.out.println(groceries);
+		groceries.sort(Comparator.reverseOrder());
+		System.out.println(groceries);
+
+		var groceryArray = groceries.toArray(new String[groceries.size()]);
+		System.out.println(Arrays.toString(groceryArray));
 	}
 }
