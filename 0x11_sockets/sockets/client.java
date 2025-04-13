f5cda5fdae138cb694cc96fc1d63689ba44175b1
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-04-14 00:01:22 +0200

    feat(sockets): Update client connecion.

--- 0x11_sockets/sockets/client.java
+++ 0x11_sockets/sockets/client.java
@@ -5,5 +5,18 @@ import java.io.*;
 public class client {
 	public static void main(String[] args) throws IOException {
 		Socket s = new Socket("localhost", 4999);
+
+		// client to server
+		PrintWriter pr = new PrintWriter(s.getOutputStream());
+		pr.println("is it working?");
+		pr.flush();
+
+		// server to client
+		InputStreamReader in = new InputStreamReader(s.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
+
+		String str = bf.readLine();
+		System.out.println("server : " + str);
+
 	}
 }
