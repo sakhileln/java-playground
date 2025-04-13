7badd15e59c8b5915423603bca5ae906d3ddd741
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-04-14 00:01:43 +0200

    feat(sockets): Update server connection.

--- 0x11_sockets/sockets/server.java
+++ 0x11_sockets/sockets/server.java
@@ -7,5 +7,17 @@ public class server {
 		Socket s = ss.accept();
 
 		System.out.println("client connected...");
+
+		// client to server
+		InputStreamReader in = new InputStreamReader(s.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
+
+		String str = bf.readLine();
+		System.out.println("client said : " + str);
+
+		// server to client
+		PrintWriter pr = new PrintWriter(s.getOutputStream());
+		pr.println("yes!!!");
+		pr.flush();
 	}
 }
