8c13a66afdb8fbd5b004b72ae785503f22561551
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-04-20 00:19:44 +0200

    feat(for): for loop fun.

---
+++ MaybeCool.java
@@ -0,0 +1,11 @@
+public class MaybeCool {
+	public static void main(String[] args) {
+
+		int x = 0;
+		do {
+			System.out.println(x);
+			x++;
+		} while (x != 0);
+		System.out.println("done!");
+	}
+}
