f67182b32db4575b38c8ac08a21705a083108a84
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-04-19 23:59:50 +0200

    feat(for): For loop fun.

---
+++ For.java
@@ -0,0 +1,15 @@
+public class For {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i++);
+		}
+
+		List<String> names = List.of("Katie", "Moise", "Mpho", "Sihle");
+		for (String n : names) {
+			if ("Mpho".equals(n)) break;
+			System.out.println(n);
+		}
+
+	}
+}
