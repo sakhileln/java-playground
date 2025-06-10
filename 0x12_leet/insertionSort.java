3d3052ee15d89fad8926a5d975d4b3ea58964d3a
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-06-11 01:30:17 +0200

    feat: sort using 'Collections' ascending and descending.

--- 0x12_leet/insertionSort.java
+++ 0x12_leet/insertionSort.java
@@ -1,4 +1,7 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 public class insertionSort {
   private static int[] sort(int[] numbers) {
@@ -17,5 +20,14 @@ public class insertionSort {
   public static void main(String[] args) {
     int[] nums = {4,2,1,3,9};
     System.out.println(Arrays.toString(sort(nums)));
+    
+    int[] nums2 = {2,-2,4,6,7,8,-5,20,11,14,0};
+    List<Integer> nums3 = new ArrayList<>();
+    for (int n : nums2) { nums3.add(n); }
+    Collections.sort(nums3);
+    System.out.println(nums3);
+
+    Collections.sort(nums3, Collections.reverseOrder());
+    System.out.println(nums3);
   }
 }
